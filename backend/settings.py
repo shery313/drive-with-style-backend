864294fa6a21453b17e0f6fe9acebe6fae508777
sheryar <sheryarsatti6@gmail.com>
2025-11-22 20:41:49 +0500

    all origing

--- backend/settings.py
+++ backend/settings.py
@@ -146,6 +146,7 @@ CORS_ALLOWED_ORIGINS = [
 
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-9a3af.up.railway.app",
      "http://127.0.0.1:8000/",
      "http://localhost:5173/",
      "https://web-production-9a3af.up.railway.app",
