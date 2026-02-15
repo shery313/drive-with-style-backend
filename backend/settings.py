34ff420fddba23f710694f823334f20033496b65
sheryar <sheryarsatti6@gmail.com>
2026-02-15 12:16:00 +0500

    git push

--- backend/settings.py
+++ backend/settings.py
@@ -273,8 +273,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'contact.drivewithstyle@gmail.com'
-EMAIL_HOST_PASSWORD = 'nngn uysq ccly mtom'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Owner email
