2e60fbf12476660c1a25e82d6e136efe9563e721
sheryar <sheryarsatti6@gmail.com>
2026-02-15 17:01:56 +0500

    added sendgrid configurations

--- backend/settings.py
+++ backend/settings.py
@@ -270,7 +270,7 @@ JAZZMIN_UI_TWEAKS = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER ="contact.drivewithstyle@gmail.com"
